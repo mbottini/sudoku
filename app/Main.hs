6e4fd947aac76092cf0b8fa5dc183f588eec131f
Michael Bottini <mbottini1@gmail.com>
2026-01-08 13:18:46 -0800

    Finish Main executable

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,36 @@
 module Main where
 
+import System.IO
+import System.Environment
+import Sudoku
+
+unifyFileInput :: Maybe String -> IO Handle
+unifyFileInput Nothing = return stdin
+unifyFileInput (Just "-") = return stdin
+unifyFileInput (Just path) = openFile path ReadMode
+
+getPath :: IO (Maybe String)
+getPath =
+    do
+        args <- getArgs 
+        case args of
+            [] -> return Nothing
+            (path:_) -> return (Just path)
+
+getHandle :: IO Handle
+getHandle = getPath >>= unifyFileInput
+    
+printPuzzle :: [Mark] -> IO ()
+printPuzzle =
+    putStrLn . showSolvedPuzzle
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    handle <- getHandle
+    solutions <- solvePuzzle . lines <$> hGetContents handle
+    putStrLn ("There " ++ 
+        (if length solutions == 1 then "is " else "are ") ++
+        (show . length $ solutions) ++ 
+        (if length solutions == 1 then " solution" else " solutions"))
+    mapM_ (putStrLn . showSolvedPuzzle) solutions
+    hClose handle
